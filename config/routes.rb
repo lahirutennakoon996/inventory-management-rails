4acd360ff032c0a734d927954b3664a997122f72
Lahiru Tennakoon <lahiru@speralabs.com>
2022-07-14 00:16:03 +0530

    Created product controller and model files only.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
 
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "products#index"
+
+  get "/products", to: "products#index"
 end
