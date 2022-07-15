565b97cfa78d13db58ca73b926fa26e63393895d
Lahiru Tennakoon <lahiru@speralabs.com>
2022-07-15 23:49:57 +0530

    Added product creation form.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,15 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
+  end
+
+  def create
+
   end
 end
