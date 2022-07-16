01c6682a5f763bda692f4853c0c53a957e1de6c0
Lahiru Tennakoon <lahirutennakoon996>
2022-07-16 23:36:30 +0530

    Create and update product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,30 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
 
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :quantity)
   end
 end
